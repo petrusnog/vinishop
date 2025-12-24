f1241fa10e5c085f949a0f31350e2aa6ea82bd48
Petrus Nogueira <petrusyagami@gmail.com>
2025-12-24 00:13:02 -0300

    feat: creating products and product_variants models #1

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'title',
+        'category',
+        'base_price',
+        'description',
+    ];
+
+    const CATEGORY_SHIRT = 'shirt';
+    const CATEGORY_CAP = 'cap';
+    const CATEGORY_MUG = 'mug';
+
+    public static function getCategories(): array
+    {
+        return [
+            self::CATEGORY_SHIRT => 'Camisas',
+            self::CATEGORY_CAP => 'Bonés',
+            self::CATEGORY_MUG => 'Canecas',
+        ];
+    }
+}
---
+++ database/migrations/2025_12_24_024159_create_product_variants_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->enum('size', ['S', 'M', 'L', 'XL']);
+            $table->string('color');
+            $table->decimal('additional_price', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
